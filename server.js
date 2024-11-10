eebc8fe0a926619652235fbcd95b833a0df90f1d
Henrique William <os.henriquewilliam@gmail.com>
2024-11-09 21:48:48 -0300

    gitignire commit

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use(express.json());
 app.use('/products', routeProduct);
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
